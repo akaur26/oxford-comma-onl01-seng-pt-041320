1ce5bb8d821f3fb6e4f35b6d39b83b7fd7217be5
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-22 22:20:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,14 @@
+array = ["kiwi"]
 def oxford_comma(array)
- if array.size == 1 
-   array.join
-   elsif array == 2 
-   array.join(" and ")
- else
-   array[0..-2].join(",") + " and " + array[-1]
- end
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
+
 oxford_comma(array)
