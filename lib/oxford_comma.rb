450efef3f44d4af539766a726b32181ddb232200
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-22 22:15:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
  if array.size == 1 
    array.join
-   elsif array ==2 
-   array.join("and")
+   elsif array == 2 
+   array.join(" and ")
  else
-   ["simo"]
-   array[0..2]
+   array[0..-2].join(",") + " and " + array[-1]
+ end
 end
